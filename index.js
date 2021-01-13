f189927daa58d08f06c848c928b761dc0a62b4e9
rohangawhade <gawhaderohan@gmail.com>
2021-01-13 19:58:31 +0530

    Hide the configs

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const dotenv = require('dotenv');
+dotenv.config();
+const {apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId} = process.env;
+
 app.set('view engine', 'ejs')
 app.set("views", path.join(__dirname, "views/templates/"));
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {apiKey:apiKey, authDomain:authDomain, databaseURL:databaseURL, projectId:projectId, storageBucket:storageBucket, messagingSenderId:messagingSenderId, appId:appId, measurementId:measurementId});
 })
 
 app.listen(3000, () => {
